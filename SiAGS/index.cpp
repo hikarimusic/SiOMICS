d06bf3d7332edd69f7ab76a8670ac005ff8016ce
hikari <hikarimusic2002@gmail.com>
2023-03-26 23:21:08 +0800

    pack 1

--- SiAGS/index.cpp
+++ SiAGS/index.cpp
@@ -1,18 +1,50 @@
 #include <iostream>
 #include <fstream>
 #include <cstdint>
+#include <cstdlib>
 #include <string>
 
-void write_actg(std::uint32_t* ref, int pos, char nuc) {
-    return;
+#include <bitset>
+
+void write_acgt(std::uint32_t* ref, int pos, char nuc) {
+    int box = pos >> 4;
+    int pnt = (0b1111 - pos & 0b1111) << 1;
+    if (nuc == 'A' || nuc == 'a') {
+        *(ref+box) &= ~(0b01<<pnt);
+        *(ref+box) &= ~(0b10<<pnt);
+    }
+    else if (nuc == 'C' || nuc == 'c') {
+        *(ref+box) |= (0b01<<pnt);
+        *(ref+box) &= ~(0b10<<pnt);
+    }
+    else if (nuc == 'G' || nuc == 'g') {
+        *(ref+box) &= ~(0b01<<pnt);
+        *(ref+box) |= (0b10<<pnt);
+    }
+    else if (nuc == 'T' || nuc == 't') {
+        *(ref+box) |= (0b01<<pnt);
+        *(ref+box) |= (0b10<<pnt);
+    }
+    else {
+        if (std::rand()&1)
+            *(ref+box) &= ~(0b01<<pnt);
+        else
+            *(ref+box) |= (0b01<<pnt);
+        if (std::rand()&1)
+            *(ref+box) &= ~(0b10<<pnt);
+        else
+            *(ref+box) |= (0b10<<pnt);
+    }
 }
 
 void pack(std::string ref_f, std::uint32_t* ref) {
     char* read{new char[4294967296]{}};   // +4GiB
-    std::ifstream infile(ref_f, std::ios::binary);
+    std::ifstream infile;
+    infile.open(ref_f, std::ios::binary);
     infile.read((char*)read, 4294967296);
     infile.close();
-    std::ofstream outfile(ref_f+".chr", std::ios::trunc);
+    std::ofstream outfile;
+    outfile.open(ref_f+".chr", std::ios::trunc);
     int read_f{0}, nuc_s{0}, nuc_e{0};
     std::string chr_name{};
     for (int i=0; i<4294967296; ++i) {
@@ -32,10 +64,13 @@ void pack(std::string ref_f, std::uint32_t* ref) {
         else if (read_f==1)
             chr_name += *(read+i);
         else if (read_f==2 && *(read+i)!='\n') {
-            write_actg(ref, nuc_e, *(read+i));
+            write_acgt(ref, nuc_e, *(read+i));
             nuc_e += 1;
         }
     }
+    outfile.close();
+    outfile.open(ref_f+".seq", std::ios::binary);
+    outfile.write((char*)ref, sizeof(*ref) * ((nuc_e>>4)+1));
     outfile.close();
     delete[] read;   // -4GiB
 }
@@ -43,6 +78,9 @@ void pack(std::string ref_f, std::uint32_t* ref) {
 void index(std::string ref_f) {
     std::uint32_t* ref{new std::uint32_t[268435456]{}};   // +1GiB
     pack(ref_f, ref);
+    // for (int i=0; i<20; ++i) {
+    //     std::cout << std::bitset<32>(*(ref+i)) << '\n';
+    // }
     delete[] ref;   // -1GiB;
 }
 
